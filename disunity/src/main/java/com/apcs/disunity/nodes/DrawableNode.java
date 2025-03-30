f651b2be55087989f9179e3b17ad4fcb5f250620
hem1sphereCHS <ttakeuchi141@student.fuhsd.org>
2025-03-30 13:49:56 -0700

    document DrawableNode and NonDrawingNode

--- disunity/src/main/java/com/apcs/disunity/nodes/DrawableNode.java
+++ disunity/src/main/java/com/apcs/disunity/nodes/DrawableNode.java
@@ -1,12 +1,15 @@
 package com.apcs.disunity.nodes;
 
+import com.apcs.disunity.Game;
 import com.apcs.disunity.math.Vector2;
 
+/// a node that has children which may draw to a buffer.
 public abstract class DrawableNode extends Node<Node> {
   public DrawableNode() { super(); }
   public DrawableNode(Node... nodes) { super(nodes); }
 
-  /// draws content of this node.
+  /// draws content of this node to buffer.
+  /// use {@link Game#getInstance()} and {@link Game#getBuffer()} to access current buffer.
   /// @param offset where the node should be drawn
   public abstract void draw(Vector2 offset);
 }
