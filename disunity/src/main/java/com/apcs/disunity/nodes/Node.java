33a71ae4a6163ed289bee14dd729dab43ba4ccff
hem1sphereCHS <ttakeuchi141@student.fuhsd.org>
2025-04-01 22:41:59 -0700

    implement Node::print

--- disunity/src/main/java/com/apcs/disunity/nodes/Node.java
+++ disunity/src/main/java/com/apcs/disunity/nodes/Node.java
@@ -3,6 +3,7 @@ package com.apcs.disunity.nodes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * The base class for all nodes in the game
@@ -43,5 +44,37 @@ public abstract class Node<T extends Node<?>> {
         // Update children
         for (T node : children) node.update(delta);
     }
-    
+
+    /// Overload for default behavior of {@link #print(boolean, Function, List)}.
+    /// Prints node names in tree structure
+    public void print() {
+        print(
+          true,
+          n -> "○ " + n.getClass().getSimpleName(),
+          new ArrayList<>());
+    }
+    /// method that prints information and children of node recursively in a tree format.
+    /// @param isLast indicates if this node is last child of parent
+    /// @param formatter function that formats provided node to printed string
+    /// @param indent string used to indent this node
+    private void print(boolean isLast, Function<Node<?>, String> formatter, List<String> indent) {
+        indent.forEach(System.out::print);
+        System.out.print(isLast ? "└ " : "├ ");
+        System.out.println(formatter.apply(this));
+
+        if(isLast) {
+            indent.add("  ");
+        } else {
+            indent.add("│ ");
+        }
+
+        for(int i=0; i<children.size()-1; i++) {
+            ((Node<?>) children.get(i)).print(false, formatter, indent);
+        }
+        if(!children.isEmpty()) {
+            ((Node<?>) children.getLast()).print(true, formatter, indent);
+        }
+
+        indent.removeLast();
+    }
 }
