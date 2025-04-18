844700ffb71e1334353cc2db6e59d87d27d959a6
Sharvil Phadke <sharvil.phadke@gmail.com>
2025-04-18 11:57:01 -0700

    add bytepacking utils

--- disunity/src/main/java/com/apcs/disunity/server/Util.java
+++ disunity/src/main/java/com/apcs/disunity/server/Util.java
@@ -1,5 +1,6 @@
 package com.apcs.disunity.server;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import static java.lang.Byte.SIZE;
@@ -7,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.fasterxml.jackson.core.util.ByteArrayBuilder;
 
 /**
  * 
@@ -72,11 +75,54 @@ public final class Util {
         } catch (IOException ioe) { return null; }
     }
 
-    public static final byte[] getBytes(int val) {
-        byte[] bytes = new byte[Integer.BYTES];
-        for (int i = 0; i < Integer.BYTES; i++) {
-            bytes[i] = (byte) ((val & (0XFF << (i * SIZE))) >> (i * SIZE));
-        }
+    public static final byte[] getBytes(Object o) {
+        if (o instanceof Byte val) return getBytes(val);
+        if (o instanceof Short val) return getBytes(val);
+        if (o instanceof Character val) return getBytes(val);
+        if (o instanceof Integer val) return getBytes(val);
+        if (o instanceof Long val) return getBytes(val);
+        if (o instanceof Boolean val) return getBytes(val);
+        if (o instanceof Float val) return getBytes(val);
+        if (o instanceof Double val) return getBytes(val);
+        if (o instanceof Syncable val) return val.supply(0);
+        return null;
+    }
+
+    public static final byte[] getBytes(Byte val) { return new byte[] {val}; }
+
+    public static final byte[] getBytes(Short val) { return getBytes(val & ~0L, Short.BYTES); }
+
+    public static final byte[] getBytes(Character val) { return getBytes(val & ~0L, Character.BYTES); }
+
+    public static final byte[] getBytes(Integer val) { return getBytes(val & ~0L, Integer.BYTES); }
+    
+    public static final byte[] getBytes(Long val) { return getBytes(val, Long.BYTES); }
+
+    public static final byte[] getBytes(Boolean val) { return new byte[] { (byte) (val ? ~0 : 0) }; }
+
+    public static final byte[] getBytes(Float val) { return getBytes(Float.floatToRawIntBits(val)); }
+
+    public static final byte[] getBytes(Double val) { return getBytes(Double.doubleToRawLongBits(val)); }
+
+    public static final byte[] getBytes(byte val) { return new byte[] {val}; }
+
+    public static final byte[] getBytes(short val) { return getBytes(val & ~0L, Short.BYTES); }
+
+    public static final byte[] getBytes(char val) { return getBytes(val & ~0L, Character.BYTES); }
+
+    public static final byte[] getBytes(int val) { return getBytes(val & ~0L, Integer.BYTES); }
+    
+    public static final byte[] getBytes(long val) { return getBytes(val, Long.BYTES); }
+
+    public static final byte[] getBytes(boolean val) { return new byte[] { (byte) (val ? ~0 : 0) }; }
+
+    public static final byte[] getBytes(float val) { return getBytes(Float.floatToRawIntBits(val)); }
+
+    public static final byte[] getBytes(double val) { return getBytes(Double.doubleToRawLongBits(val)); }
+
+    public static final byte[] getBytes(long val, int size) {
+        byte[] bytes = new byte[size];
+        for (int i = 0; i < size; i++) bytes[i] = (byte) ((val & (~0L << (i * SIZE))) >> (i * SIZE));
         return bytes;
     }
 
@@ -90,14 +136,6 @@ public final class Util {
 
     public static final int getInt(byte[] data) {
         return getInt(data, 0);
-    }
-
-    public static final byte[] getBytes(long val) {
-        byte[] bytes = new byte[Long.BYTES];
-        for (int i = 0; i < Long.BYTES; i++) {
-            bytes[i] = (byte) ((val & (0XFFL << (i * SIZE))) >> (i * SIZE));
-        }
-        return bytes;
     }
 
     public static final long getLong(byte[] data, int loc) {
