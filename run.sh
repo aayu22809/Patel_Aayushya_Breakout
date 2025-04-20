a1759b0e397c103c91664fce05fb3aa84db242b6
hem1sphereCHS <ttakeuchi141@student.fuhsd.org>
2025-04-20 01:41:00 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-(cd processor ; mvn clean install compile)
+# author of this script had no experience with bash before
+if [ ! -d processor/target ]; then
+  (cd processor ; mvn clean install compile)
+else
+  echo "found processor; skipping install."
+fi
+
 mvn clean install compile
-mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG" -f "./ljaag/pom.xml" &
-mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml" &
-mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml" &
-mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml" &
-mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml"
+for i in {1..4} ; do
+  mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml" &
+done
+mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG" -f "./ljaag/pom.xml"
