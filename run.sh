4a15a0caff7dc866bd5917c907142bad94c9c9bf
hem1sphereCHS <ttakeuchi141@student.fuhsd.org>
2025-04-21 18:04:51 -0700

    put processor to separate repo
    repo: https://github.com/hem1sphereCHS/FieldTypeEnforcer

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # author of this script had no experience with bash before
-if [ ! -d processor/target ]; then
-  (cd processor ; mvn clean install compile)
-else
-  echo "found processor; skipping install."
-fi
-
 mvn clean install compile
 for i in {1..4} ; do
   mvn exec:java -Dexec.mainClass="com.apcs.ljaag.LJAAG\$Client" -f "./ljaag/pom.xml" &
