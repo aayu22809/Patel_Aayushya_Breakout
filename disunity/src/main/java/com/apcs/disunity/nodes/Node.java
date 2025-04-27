7d9afa4d2a3ca89075af81cb5a0c466a860e1160
aayu22809 <aayu22809@gmail.com>
2025-04-27 16:04:28 -0700

    Update Node class to support parent/children

--- disunity/src/main/java/com/apcs/disunity/nodes/Node.java
+++ disunity/src/main/java/com/apcs/disunity/nodes/Node.java
@@ -1,5 +1,6 @@
 package com.apcs.disunity.nodes;
 
+import com.apcs.disunity.annotations.Requires;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -7,15 +8,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
-import com.apcs.disunity.annotations.Requires;
-
 /**
  * The base class for all nodes in the game
- * 
+ *
  * @author Qinzhao Li
  * @author Toshiki Takeuchi
  */
 public abstract class Node<T extends Node<?>> {
+
     // MUST DO: remove this
     public int owner;
 
@@ -24,34 +24,57 @@ public abstract class Node<T extends Node<?>> {
     // List of children
     private final List<T> children;
 
+    // Parent of node
+    private Node<?> parent;
+
     // Triggers initialize
     private boolean isInitialized = false;
 
     // Constructors
-    public Node() { this.children = new ArrayList<>(); }
+    public Node() {
+        this.children = new ArrayList<>();
+    }
+
     @SafeVarargs
-    public Node(T... children) { this.children = new ArrayList<>(Arrays.asList(children)); }
+    public Node(T... children) {
+        this.children = new ArrayList<>(Arrays.asList(children));
+    }
 
     /* ================ [ METHODS ] ================ */
 
     // Add child
     public void addChild(T node) {
         children.add(node);
+        node.setParent(this);
         isInitialized = false;
+    }
+
+    // Get parent
+    public Node<?> getParent() {
+        return parent;
+    }
+
+    // Set parent
+    public void setParent(Node<?> parent) {
+        this.parent = parent;
     }
 
     @SafeVarargs
     public final void addChildren(T... nodes) {
-        for(T child: nodes) {
+        for (T child : nodes) {
             addChild(child);
         }
     }
 
     // Remove child
-    public void removeChild(T node) { children.remove(node); }
+    public void removeChild(T node) {
+        children.remove(node);
+    }
 
     // Clear children
-    public void clearChildren() { children.clear(); }
+    public void clearChildren() {
+        children.clear();
+    }
 
     // Get child of a certain type
     public <U extends T> U getChild(Class<U> type) {
@@ -59,11 +82,14 @@ public abstract class Node<T extends Node<?>> {
             if (type.isInstance(node)) {
                 return type.cast(node);
             }
-        } return null;
+        }
+        return null;
     }
 
     // Get children
-    public List<T> getChildren() { return children; }
+    public List<T> getChildren() {
+        return children;
+    }
 
     // Get children of a certain type
     public <U> List<U> getChildren(Class<U> type) {
@@ -72,7 +98,8 @@ public abstract class Node<T extends Node<?>> {
             if (type.isInstance(node)) {
                 children.add(type.cast(node));
             }
-        } return children;
+        }
+        return children;
     }
 
     /* ================ [ NODE ] ================ */
@@ -83,18 +110,24 @@ public abstract class Node<T extends Node<?>> {
         if (this.getClass().isAnnotationPresent(Requires.class)) {
             // Grab nodes from annotation
             Requires requires = this.getClass().getAnnotation(Requires.class);
-            Set<Class<? extends Node>> requirements = new HashSet<>(Arrays.asList(requires.nodes()));
+            Set<Class<? extends Node>> requirements = new HashSet<>(
+                Arrays.asList(requires.nodes())
+            );
 
             // Check children
             for (T node : children) {
-                requirements.removeIf(required -> required.isAssignableFrom(node.getClass()));
+                requirements.removeIf(required ->
+                    required.isAssignableFrom(node.getClass())
+                );
             }
 
             // Throw exception if requirements are not met
             if (!requirements.isEmpty()) {
                 throw new RuntimeException(
-                    "Node " + this.getClass().getSimpleName() +
-                    " requires " + requirements.iterator().next().getSimpleName() + 
+                    "Node " +
+                    this.getClass().getSimpleName() +
+                    " requires " +
+                    requirements.iterator().next().getSimpleName() +
                     " to be initialized."
                 );
             }
@@ -125,9 +158,9 @@ public abstract class Node<T extends Node<?>> {
     /// Prints node names in tree structure
     public void print() {
         print(
-          true,
-          n -> "○ " + n.getClass().getSimpleName(),
-          new ArrayList<>()
+            true,
+            n -> "○ " + n.getClass().getSimpleName(),
+            new ArrayList<>()
         );
     }
 
@@ -135,7 +168,11 @@ public abstract class Node<T extends Node<?>> {
     /// @param isLast indicates if this node is last child of parent
     /// @param formatter function that formats provided node to printed string
     /// @param indent string used to indent this node
-    private void print(boolean isLast, Function<Node<?>, String> formatter, List<String> indent) {
+    private void print(
+        boolean isLast,
+        Function<Node<?>, String> formatter,
+        List<String> indent
+    ) {
         indent.forEach(System.out::print);
         System.out.print(isLast ? "└ " : "├ ");
         System.out.println(formatter.apply(this));
@@ -152,5 +189,4 @@ public abstract class Node<T extends Node<?>> {
 
         indent.removeLast();
     }
-
 }
