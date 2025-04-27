96ae2cc413ebd5a2439529dc53ce600628c77801
aayu22809 <aayu22809@gmail.com>
2025-04-27 16:02:12 -0700

    Add proprietary Rectangle class for collision detection

---
+++ disunity/src/main/java/com/apcs/disunity/physics/Rectangle.java
@@ -0,0 +1,55 @@
+package com.apcs.disunity.physics;
+
+import com.apcs.disunity.math.Vector2;
+
+/**
+ * A rectangle for collision detection
+ */
+public class Rectangle {
+
+    private final Vector2 position; // Top-left corner
+    private final Vector2 size;
+
+    public Rectangle(Vector2 position, Vector2 size) {
+        this.position = position;
+        this.size = size;
+    }
+
+    /**
+     * Check if this rectangle intersects with another
+     */
+    public boolean intersects(Rectangle other) {
+        // Check if one rectangle is to the left of the other
+        if (
+            position.x + size.x < other.position.x ||
+            other.position.x + other.size.x < position.x
+        ) {
+            return false;
+        }
+
+        // Check if one rectangle is above the other
+        if (
+            position.y + size.y < other.position.y ||
+            other.position.y + other.size.y < position.y
+        ) {
+            return false;
+        }
+
+        // If neither, the rectangles intersect
+        return true;
+    }
+
+    /**
+     * Get the position of the rectangle
+     */
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    /**
+     * Get the size of the rectangle
+     */
+    public Vector2 getSize() {
+        return size;
+    }
+}
